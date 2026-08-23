d4c4208c0186bb5f303d73a7925ed64273e8718a
Drago <great1.abhi@gmail.com>
2026-08-24 03:31:01 +0530

    Resolved: TypeError in NavbarDesktop component. Added: .env.example

--- src/components/navbar/NavbarDesktopLayout.tsx
+++ src/components/navbar/NavbarDesktopLayout.tsx
@@ -1,13 +1,12 @@
-import { useCallback } from 'react';
 import { Navbar } from '@components';
 import styles from './NavbarDesktopLayout.module.css'
 
 function NavbarDesktopLayout () {
     const showNavbar = true;
-    const setShowNavbar = useCallback(() => {showNavbar}, []);
+    const setShowNavbar = () => {showNavbar};
     return (
         <nav className={styles['desktop-navbar']}>
-            <Navbar setShowNavbar={setShowNavbar} className={styles['desktop-navbar-main']}/>
+            <Navbar closeNavbar={setShowNavbar} className={styles['desktop-navbar-main']}/>
         </nav>
     )
 }
